8ccbdc09843c245b9cdafd6933446d7f6b820d72
shencanet <santi.dionis@gmail.com>
2022-08-08 13:02:39 +0200

    add: productos tienda online

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import Amanda from './components/Amanda';
-import Emma from './components/Emma';
-import Shawn from './components/Shawn';
-import Navegacion from './components/Navegacion';
+
 
 
 function App() {
   return (
- <Router>
-  <Route path='/Amanda' component={Amanda} />
-  <Route path='/Emma' component={Emma} />
-  <Route path='/Shawn' component={Shawn} />
-  <Navegacion/>
- </Router>
+<div className='App'>
+  <h1>Hola mundo cruel</h1>
+</div>
 
   );
 }
---
+++ src/components/productos/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+esport const productosLista = () => {
+  return (
+    <div>index</div>
+  )
+}
+
